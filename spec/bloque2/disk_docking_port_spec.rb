bfee516aa87be06c5338e31ba47d6f0cbd246b6c
Fernando Garcia Samblas <fernando.garcia@the-cocktail.com>
2014-12-03 15:53:51 +0100

    should persist landing reports

--- spec/bloque2/disk_docking_port_spec.rb
+++ spec/bloque2/disk_docking_port_spec.rb
@@ -13,4 +13,11 @@ describe DiskDockingPort do
     launcher2.cruising.must_include just_launched
   end
  
+  it 'should persist landing reports between different Launching instances' do
+    score = 77
+    @launcher.just_landed! @first_launch, score
+    launcher2 = Launcher.new(:disk)
+    launcher2.landed.must_include @first_launch
+    launcher2.score(@first_launch).must_equal score
+  end
 end
