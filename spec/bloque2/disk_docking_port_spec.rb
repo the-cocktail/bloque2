00d86664cd2b2dc94e7324532c8271e34f2a2b3d
Fernando Garcia Samblas <fernando.garcia@the-cocktail.com>
2014-12-03 19:09:44 +0100

    Added missed assertions to DiscDockingPort

--- spec/bloque2/disk_docking_port_spec.rb
+++ spec/bloque2/disk_docking_port_spec.rb
@@ -8,16 +8,22 @@ describe DiskDockingPort do
   end
 
   it 'should persist launchings between different Launching instances' do
+    pending, landed = @launcher.pending, @launcher.landed
     just_launched = @launcher.launch_spacecraft!
     launcher2 = Launcher.new(:disk)
     launcher2.cruising.must_include just_launched
+    launcher2.pending.size.must_equal pending.size - 1
+    launcher2.landed.size.must_equal landed.size
   end
  
   it 'should persist landing reports between different Launching instances' do
+    pending, landed = @launcher.pending, @launcher.landed
     score = 77
     @launcher.just_landed! @first_launch, score
     launcher2 = Launcher.new(:disk)
     launcher2.landed.must_include @first_launch
     launcher2.score(@first_launch).must_equal score
+    launcher2.pending.size.must_equal pending.size
+    launcher2.landed.size.must_equal landed.size + 1
   end
 end
